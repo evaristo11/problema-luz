8611b005e19618d23d269b4525ac40d6ae8869d4
Evaristo <a01177175@itesm.mx>
2021-03-18 17:51:39 -0600

    ya esta la toma de las variables del codigo

---
+++ codigo.py
@@ -0,0 +1,13 @@
+
+hora.inicio = int(input(" ¿ Que hora es ? (horario militar 24h)"))
+#Variable de hora solo toma numeros del 1 al 24
+
+hora.encendido = int(input(" ¿ A que hora quiere que se encienda la luz ? (horario militar 24h)"))
+#variable de prendidio de luz solo toma numeros del 1 al 24
+
+hora.apagado = int(input(" ¿ A que hora quiere que se apage la luz ? (horario militar 24h)"))
+
+#variables de apagado de la luz solo toma numeros del 1 al 24
+
+
+
